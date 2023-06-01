d49b830ad8b0d3deeb5882b936097192ee8efbb2
Thomas Lau <tomslau@outlook.com>
2023-05-31 20:37:12 -0700

    readme

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Compass from './Sensor';
+import Light from './Light';
+import Button from './Button';
 export default function App() {
+  const [showComponent, setShowComponent] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ fontSize: 25 }}>I am bored...</Text>
       <StatusBar style="auto" />
-    </View>
+      <Button
+        label="Surprise Me!"
+        show={setShowComponent}
+        status={showComponent}
+      />
+
+      {showComponent && (
+        <>
+          <View style={{ flex: 1 / 3, alignContent: 'center', width: 300 }}>
+            <Compass />
+          </View>
+          <View style={{ flex: 1 / 4, alignContent: 'center', width: 300 }}>
+            <Light />
+          </View>
+        </>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ddffd6',
     alignItems: 'center',
     justifyContent: 'center',
+    alignContent: 'center',
   },
 });
